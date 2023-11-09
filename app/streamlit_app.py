f14f4631008740e12ceacb602f53deed71d2ef87
Kritz23 <kritz2399@gmail.com>
2023-11-09 14:18:46 +1100

    restructure streamlit code

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
-import sys
 import os
-sys.path.append("./src/features")
 
 st.title('Flight Fare Estimation App')
 st.write('Enter your trip details below:')
@@ -23,30 +21,17 @@ new_data = {
 inf_df = pd.DataFrame([new_data])
 
 if st.button('Predict Fare'):
-    # import function from build_features python script 
-    from build_features import get_date_features, get_time_features
-    inf_df = get_date_features(inf_df)
-    inf_df = get_time_features(inf_df)
+    # import function from build_features python script
+    from model_tf import model_tf_prediction 
 
-    import joblib
-    from tensorflow.keras.models import load_model
+    predictions = {
+        'TensorFlow': model_tf_prediction(inf_df),
+        #'Catboost': model_catboost_prediction(inf_df),
+        #'Model 3': model_custom1_prediction(inf_df),
+        #'Model 4': model_custom2_prediction(inf_df)
+    }
+    st.write(f'Predicted fares for your {origin} to {destination} trip on {departure_date}:')
 
-    # Load the trained model
-    model = load_model("./models/Kritika/exp2_best_model.h5")
-    if model:
-        st.write('Trip Cost prediction model has been loaded successfully!')
-
-    # Load label encoders and scaler
-    label_encoder = joblib.load('./models/Kritika/label_encoder.joblib')
-    scaler = joblib.load('./models/Kritika/standard_scaler.joblib')
-
-    # Apply label encoding on categorical columns in the new data
-    for col in ['startingAirport', 'destinationAirport', 'cabin_type', 'departure_time_category']:
-        inf_df[col] = label_encoder[col].transform(inf_df[col])
-
-    # Scale the new data using the scaler fitted on the training data
-    df_scaled = scaler.transform(inf_df)
-
-    # Make prediction using the loaded model
-    prediction = model.predict(df_scaled)
-    st.write(f'Predicted fare for your {origin} to {destination} trip is: {prediction[0][0]}')
+    # Display predictions in a table
+    predictions_df = pd.DataFrame(predictions.items(), columns=['Model', 'Predicted Fare'])
+    st.write(predictions_df)
